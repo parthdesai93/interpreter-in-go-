000acf522123c9790a6daba099f46a5411ee4f95
Parth Desai <psdesai.93@gmail.com>
2018-07-24 21:26:02 -0400

    add lexer tests

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,29 +1,8 @@
 package lexer
 
-import (
-	"testing"
-
-	"token"
-)
-
-func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-
-	tests := []struct {
-		expectedToken  token.TokenType
-		expectedLitera string
-	}{
-		{token.ASSIGN, "="},
-	}
-
-	l := New(input)
-
-	for l, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.expectedToken {
-			t.Fatal(`tokentype wrong`)
-		}
-
-		if tok.Literal !=
-	}
+type Lexer struct {
+	input 				string
+	position 			int
+	readPosition 	int
+	ch 						byte
 }
